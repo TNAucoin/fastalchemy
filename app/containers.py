bcf11b64212923416b6c1c5ebd4ea7849c50a6b8
TravisAucoin <travis_aucoin@stuller.com>
2022-04-24 19:23:03 -0500

    Update containers.py
    refactor container service code

--- app/containers.py
+++ app/containers.py
@@ -1,23 +1,13 @@
 from dependency_injector import containers, providers
+from dependency_injector.providers import Singleton, Factory
 
 from app.api.v1.endpoints import users
 from app.database import Database
 from app.repositories import UserRepository, TodoRepository
-from app.services import UserService
+from app.services import UserService, TodoService
 
 
-class Container(containers.DeclarativeContainer):
-    wiring_config = containers.WiringConfiguration(modules=[users])
-    config = providers.Configuration(yaml_files=["config.yml"])
-    db = providers.Singleton(Database, db_url=config.db.url)
-    todo_repository = providers.Factory(
-        TodoRepository,
-        session_factory=db.provided.session,
-    )
-    todo_service = providers.Factory(
-        UserService,
-        todo_repository=todo_repository
-    )
+def create_user_service(db: Singleton[Database]) -> Factory[UserService]:
     user_repository = providers.Factory(
         UserRepository,
         session_factory=db.provided.session,
@@ -26,3 +16,24 @@ class Container(containers.DeclarativeContainer):
         UserService,
         user_repository=user_repository
     )
+    return user_service
+
+
+def create_todo_service(db: Singleton[Database]) -> Factory[TodoService]:
+    todo_repository = providers.Factory(
+        TodoRepository,
+        session_factory=db.provided.session,
+    )
+    todo_service = providers.Factory(
+        TodoService,
+        todo_repository=todo_repository
+    )
+    return todo_service
+
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(modules=[users])
+    config = providers.Configuration(yaml_files=["config.yml"])
+    db = providers.Singleton(Database, db_url=config.db.url)
+    todo_service = create_todo_service(db)
+    user_service = create_user_service(db)
